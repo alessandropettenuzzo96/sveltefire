38315d2b5dcd87e03fffca1d5b969ef49bf90d1e
Alessandro <ale@IPRODIAESSANDRO.station>
2025-01-21 18:55:05 +0100

    upgraded to svelte 5

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: sveltekit(),
 
 	kit: {
 		adapter: adapter()
