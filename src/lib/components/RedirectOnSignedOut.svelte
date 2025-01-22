2dd07f78acd36f8af55e49ea9a9aa9a2ac9f8cf5
Alessandro <ale@IPRODIAESSANDRO.station>
2025-01-22 15:47:33 +0100

    fixing serverside effect

--- src/lib/components/RedirectOnSignedOut.svelte
+++ src/lib/components/RedirectOnSignedOut.svelte
@@ -3,19 +3,18 @@
     import { userStore } from "../stores/auth.js";
     import type { Auth } from 'firebase/auth';
     import {goto} from "$app/navigation";
+    import {onMount} from "svelte";
 
     const auth = getFirebaseContext().auth!;
     const user = userStore(auth)
 
     const { url } = $props();
 
-    user.subscribe((user) => {
-      if (user) {
-        console.log('User is signed in');
-      } else {
-        console.log('User is signed out');
-        goto(url);
-      }
+    onMount(() => {
+      const unsubscribe = user.subscribe((user) => {
+        if (!user) goto(url);
+      });
+      return () => unsubscribe();
     });
 
   </script>
