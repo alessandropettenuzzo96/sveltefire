61e94714955bc5bd82cdc235f97bf69c55c9d237
Alessandro <ale@IPRODIAESSANDRO.station>
2025-01-22 15:24:03 +0100

    added redirects

---
+++ src/lib/components/RedirectOnSignedIn.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { getFirebaseContext } from "../stores/sdk.js";
+    import { userStore } from "../stores/auth.js";
+    import type { Auth } from 'firebase/auth';
+    import {goto} from "$app/navigation";
+
+    const auth = getFirebaseContext().auth!;
+    const user = userStore(auth)
+
+    const { url } = $props();
+
+    user.subscribe((user) => {
+      if (user) {
+        console.log('User is signed in');
+        goto(url);
+      } else {
+        console.log('User is signed out');
+      }
+    });
+
+  </script>
